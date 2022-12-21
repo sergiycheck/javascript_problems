f8e5427148dd8ae0af203da8c04673cd61e2e128
serhii <sergiychech@gmail.com>
2022-12-21 12:11:58 +0200

    learning, debugging maxProduct subArr solution

--- testing_ts_problems/src/leecodeProblems/medium/max_prduct_subarray.ts
+++ testing_ts_problems/src/leecodeProblems/medium/max_prduct_subarray.ts
@@ -1,29 +1,65 @@
 // problem
 // https://leetcode.com/problems/maximum-product-subarray/description/
 
+// export function maxProduct(nums: number[]): number {
+//   let maxProduct = nums[0];
+
+//   const areContiguos = (first: number, second: number) => second - first === 1;
+
+//   for (let i = 0; i < nums.length - 1; i++) {
+//     let firstNum = nums[i];
+//     let currentProduct = firstNum;
+
+//     for (let j = i + 1; j < nums.length; j++) {
+//       const secondNum = nums[j];
+//       if (areContiguos(firstNum, secondNum)) {
+//         currentProduct *= secondNum;
+//         firstNum = secondNum;
+//       } else {
+//         if (currentProduct > maxProduct) {
+//           maxProduct = currentProduct;
+//         }
+//         i = j - 1;
+//         break;
+//       }
+//     }
+//   }
+
+//   return maxProduct;
+// }
+
 export function maxProduct(nums: number[]): number {
-  let maxProduct = nums[0];
+  const length = nums.length;
+  if (length == 0) return 0;
 
-  const areContiguos = (first: number, second: number) => second - first === 1;
+  const maxArr = new Array(length);
+  const minArr = new Array(length);
 
-  for (let i = 0; i < nums.length - 1; i++) {
-    let firstNum = nums[i];
-    let currentProduct = firstNum;
+  maxArr[0] = nums[0];
+  minArr[0] = nums[0];
 
-    for (let j = i + 1; j < nums.length; j++) {
-      const secondNum = nums[j];
-      if (areContiguos(firstNum, secondNum)) {
-        currentProduct *= secondNum;
-        firstNum = secondNum;
-      } else {
-        if (currentProduct > maxProduct) {
-          maxProduct = currentProduct;
-        }
-        i = j - 1;
-        break;
-      }
-    }
+  for (let i = 1; i < length; i++) {
+    const currentNum = nums[i];
+
+    const maxArrPrevMultipliedWithCurrentNum = maxArr[i - 1] * currentNum;
+    const minArrPrevMultipliedWithCurrentNum = minArr[i - 1] * currentNum;
+
+    const innerMax = Math.max(
+      maxArrPrevMultipliedWithCurrentNum,
+      minArrPrevMultipliedWithCurrentNum
+    );
+    maxArr[i] = Math.max(innerMax, currentNum);
+
+    const innerMin = Math.min(
+      maxArrPrevMultipliedWithCurrentNum,
+      minArrPrevMultipliedWithCurrentNum
+    );
+    minArr[i] = Math.min(innerMin, currentNum);
   }
 
-  return maxProduct;
+  let result = nums[0];
+  for (let i = 1; i < length; i++) {
+    result = Math.max(result, maxArr[i]);
+  }
+  return result;
 }
