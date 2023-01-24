c07d97744480a2feb4fddd8aab3de860eaaf3540
serhii <sergiychech@gmail.com>
2023-01-24 18:43:29 +0200

    hash table represantation

---
+++ testing_ts_problems/src/algorithms/data-structures/hash-table.ts
@@ -0,0 +1,41 @@
+class HashTable {
+
+  values: object;
+  length: number;
+  size: number;
+
+  constructor() {
+    this.values = {};
+    this.length =  0;
+    this.size =  0;
+  }
+
+  calculateHash(key: string) {
+    return key.toString().length % this.size;
+  }
+
+  add(key: string, value: any) {
+    const hash = this.calculateHash(key);
+
+    if (!this.values.hasOwnProperty(hash)) {
+      this.values[hash] = {};
+    }
+
+    if (!this.values[hash].hasOwnProperty(key)) {
+      this.length++;
+    }
+
+    this.values[hash][key] = value;
+  }
+
+  search(key: string) {
+    const hash = this.calculateHash(key);
+    
+    if (this.values.hasOwnProperty(hash) && this.values[hash].hasOwnProperty(key)) {
+      return this.values[hash][key];
+    } else {
+      return null;
+    }
+ }
+
+ }
